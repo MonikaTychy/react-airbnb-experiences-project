56010c5655100cc407f0cc8ee756f56c3935ab79
MonikaTychy <mo.tychy@gmail.com>
2022-10-24 21:16:06 +0200

    applied styling to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,15 @@ import star from '../images/star.png';
 export default function Card() {
     return (
         <section className='card'>
-          <img src={hero1} alt='swimmer' className='card--photo' />
-          <img  src={star} alt='little red star' className='card--star' />
-          <p>5.0 (6)•USA</p>
+          <img src={hero1} alt='swimmer' className='card-photo' />
+           <div className='card-statistic'>
+            <img  src={star} alt='little red star' className='card-star' />
+            <span className='card-rating'>5.0</span>
+            <span className='card-reviews'>(6) • </span>
+            <span className='card-location'>USA</span>
+           </div>
           <p>Life lessons with Katie Zaferes</p>
-          <p>From $136 / person</p>
+          <p><span className='card-price'>From $136</span> / person</p>
         </section>
     )
 }
