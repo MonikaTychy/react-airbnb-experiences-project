d4f27103e0b5450effa91260fa76460ee2317a1f
MonikaTychy <mo.tychy@gmail.com>
2022-10-24 16:46:31 +0200

    styled hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,10 @@ import group from '../images/group.png';
 
 export default function Hero() {
     return (
-        <div className='hero'>
+        <section className='hero'>
          <img src={group} alt='group of pictures of diffrent peoples' />
          <h1>Online Experiences</h1>
          <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
-        </div>
+        </section>
     )
 }
